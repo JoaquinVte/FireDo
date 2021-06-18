513df2d3718fcdfc4015ffd75b48c7c8736f0a22
Joaquin Vicente Alonso Saiz <joaalsai@gmail.com>
2021-06-18 11:36:24 +0200

    Inicial commit

--- FireDo/ViewController.swift
+++ FireDo/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var entrarRegistrarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginRegisterSwitchAction(_ sender: UISwitch) {
+        if(sender.isOn){
+            entrarRegistrarButton.setTitle("Entrar", for: .normal)
+        } else {
+            entrarRegistrarButton.setTitle("Registrar", for: .normal)
+        }
+    }
+    
 }
 
